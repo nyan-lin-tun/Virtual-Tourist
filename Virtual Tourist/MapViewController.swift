c4b3ed49954b55ce75a75ccc875ca8d83a2c67fb
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-06-07 00:47:35 +0630

    Create core data model for location and photo

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -19,7 +19,6 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        // Do any additional setup after loading the view.
     }
 
     
---
+++ Virtual Tourist/Model/DataController.swift
@@ -0,0 +1,10 @@
+//
+//  DataController.swift
+//  Virtual Tourist
+//
+//  Created by Nyan Lin Tun on 06/06/2020.
+//  Copyright © 2020 Nyan Lin Tun. All rights reserved.
+//
+
+import Foundation
+
