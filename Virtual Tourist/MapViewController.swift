9c13b92ae70f06603389dfde3defc13a73dbef90
Nyan Lin Tun <nyanlynntunn@gmail.com>
2020-06-06 12:47:13 +0630

    Added app icon

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Nyan Lin Tun on 25/05/2020.
+//  Copyright © 2020 Nyan Lin Tun. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var pinAnnotation: MKPointAnnotation?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func addPinAction(_ sender: UILongPressGestureRecognizer) {
+        
+        let location = sender.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        if sender.state == .began {
+            
+        }
+        print("Press")
+//       switch sender.state {
+//       case .began:
+//           let pressMapCoordinate = mapView.convert(sender.location(in: mapView), toCoordinateFrom: mapView)
+//           createPin(forCoordinate: pressMapCoordinate)
+//       default:
+//           break
+//       }
+   }
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
